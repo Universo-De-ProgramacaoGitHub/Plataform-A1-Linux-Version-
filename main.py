34d107c44b6e2b101bd0b1e87a8b04abb05c0ae7
Universo-De-ProgramacaoGitHub <115424127+Universo-De-ProgramacaoGitHub@users.noreply.github.com>
2022-10-11 22:43:06 -0300

    Main.py Criado
    O Código principal foi feito na metade

---
+++ main.py
@@ -0,0 +1,17 @@
+import arcade
+
+# Set constants for the screen size
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Open the window. Set the window title and dimensions (width and height)
+arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Plataform A1")
+
+def setup(self):
+    pass
+
+def on_draw(self):
+SPRITE_SCALING_PLAYER = 1.0
+
+coin = arcade.Sprite("coin_01.png", SPRITE_SCALING_COIN)
+    pass
