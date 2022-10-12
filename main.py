424c268e2af0a733108f35d9f697fa46b493c7b5
Universo-De-ProgramacaoGitHub <115424127+Universo-De-ProgramacaoGitHub@users.noreply.github.com>
2022-10-11 23:26:34 -0300

    Lets try other day
    aff

--- main.py
+++ main.py
@@ -7,11 +7,31 @@ SCREEN_HEIGHT = 600
 # Open the window. Set the window title and dimensions (width and height)
 arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Plataform A1")
 
-def setup(self):
-    pass
-
 def on_draw(self):
 SPRITE_SCALING_PLAYER = 1.0
 
-coin = arcade.Sprite("coin_01.png", SPRITE_SCALING_COIN)
+player = arcade.Sprite("player.png", SPRITE_SCALING_PLAYER)
+
+SPRITE_SCALING_GRAMA = 1.0
+
+grama = arcade.Sprite("grama.png", SPRITE_SCALING_GRAMA)
+
+SPRITE_SCALING_BLOCO = 1.0
+
+bloco = arcade.Sprite("bloco.png", SPRITE_SCALING_BLOCO)
     pass
+
+def setup(self):
+    """ Set up the game and initialize the variables. """
+
+    # Create the sprite lists
+    self.player_list = arcade.SpriteList()
+    self.grama_list = arcade.SpriteList()
+    self.bloco_list = arcade.SpriteList()
+    
+    # Set Up The Player
+    # Character image from kenney.nl
+    self.player_sprite = arcade.Sprite("player.png", SPRITE_SCALING_PLAYER)
+    self.player_sprite.center_x = 50 # Starting position
+    self.player_sprite.center_y = 50
+    self.player_list.append(self.player_sprite)
